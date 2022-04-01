afdc68106d6ce4c91f11b5821492f1be586b1dfe
asmaa-elsayed <asmaa.ahmed537@yahoo.com>
2022-04-01 11:20:40 +0200

    vars file

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+}
+variable "instance_type" {
+}
+variable "key_name" {
+}
+variable "public_key" {
+}
+variable "vpc_id" {
+}
+variable "ec2_tag" {
+}
+variable "sg_tag" {
+}
+variable "region" {
+}
